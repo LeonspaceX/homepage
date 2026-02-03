626a1878205e0cf51653f7543243ccc45c359cd5
LeonspaceX <xiezhengyang112022@163.com>
2026-02-03 11:43:31 +0800

    更新视奸websocket地址

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return `${m}:${s.toString().padStart(2,'0')}`;
     }
 
-    let ws = new WebSocket("wss://monitor.nekoqwq.space/ws");
+    let ws = new WebSocket("wss://monitor.mahiro.space/ws");
     ws.onmessage = function(event) {
       document.getElementById("leonxie-status").innerText = event.data;
     };
